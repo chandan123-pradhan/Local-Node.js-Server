27abe5997364aa5e23c8ab91959e3811fd7d1341
chandan pradhan <chandanpradhan@smcindiaonline.com>
2024-01-31 00:01:11 +0530

    learning to map array and assign value from array to the html file and return that list

--- app.js
+++ app.js
@@ -12,6 +12,17 @@ const http=require('http');
 const fs=require('fs');
 
 let html=fs.readFileSync('./Templetes/index.html','utf-8');
+let products=JSON.parse(fs.readFileSync('./Data/products.json','utf-8')).products
+let productListHtml=fs.readFileSync('./Data/product_list.html','utf-8');
+
+let productHtmlArray=products.map((prod)=>{
+    let output=productListHtml.replace('{{%NAME%}}',prod.name);
+    output=output.replace('{{%BRAND%}}',prod.brand);
+    output=output.replace('{{%PRICE%}}',prod.price);
+    output= output.replace('{{%CATEGORY%}}',prod.category);
+   
+    return output;
+})
 
 const server=http.createServer((request, response)=>{
     // response.end(html) //sending index.html file content to the server
@@ -19,28 +30,50 @@ const server=http.createServer((request, response)=>{
     let message='';
     switch(path.toLowerCase()){
         case '/':
-            response.writeHead(200);
+            response.writeHead(200,{
+                'content-type':'text/html',
+                'custom-header':'Hello World'
+            });
             message='You are on home page';
+            response.end(message);
             break;
         case '/home':
-            response.writeHead(200);
+            response.writeHead(200,{
+                'content-type':'text/html',
+                'custom-header':'Hello World'
+            });
             message='You are on home page';
+            response.end(message);
             break;
         case '/about':
-            response.writeHead(200);//setting status code.
+            response.writeHead(200,{
+                'content-type':'text/html',
+                'custom-header':'Hello World'
+            });//setting status code.
             message='You are on about section page';
+            response.end(message);
             break;
+            case '/products':
+                response.writeHead(200,{
+                    'content-type':'text/html',
+                });//setting status code.
+                // response.end('you are in product page');
+                let productRes=html.replace('{{%CONTENT%}}',productHtmlArray.join(','))
+                response.end(productRes);
+                console.log(productRes);
+                break;
         default:
             response.writeHead(404);
             message='Not Found';
+            response.end(message);
             break;
             
     }
-    response.end(message);
+    
 
 });
 
-server.listen(8000,'127.0.0.1',()=>{
+server.listen(8001,'127.0.0.1',()=>{
     console.log("Server has started");
 });
 
