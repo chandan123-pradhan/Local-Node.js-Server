26bc8e39068dbfa23990b226e7ce624e6183cd77
chandan pradhan <chandanpradhan@smcindiaonline.com>
2024-01-30 11:43:52 +0530

    sending a file content into server

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 const http=require('http');
 
+// const server=http.createServer((request, response)=>{
+//     response.end("Hello Welcome To The Dart Place.")
+//     console.log("A New Request Recived");
+//     console.log(request);
+// });
+
+
+
+//getting a html file content and sending to the server
+const fs=require('fs');
+
+let html=fs.readFileSync('./Templetes/index.html','utf-8');
+
 const server=http.createServer((request, response)=>{
-    response.end("Hello Welcome To The Dart Place.")
-    console.log("A New Request Recived");
+    response.end(html)
     console.log(request);
 });
 
