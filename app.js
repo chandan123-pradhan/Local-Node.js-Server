8d42a7dd3ad569ab8800775ae19c803924085e55
chandan pradhan <chandanpradhan@smcindiaonline.com>
2024-01-30 17:01:54 +0530

    learning how to add status code in response header params

--- app.js
+++ app.js
@@ -14,8 +14,30 @@ const fs=require('fs');
 let html=fs.readFileSync('./Templetes/index.html','utf-8');
 
 const server=http.createServer((request, response)=>{
-    response.end(html)
-    console.log(request);
+    // response.end(html) //sending index.html file content to the server
+    let path=request.url;
+    let message='';
+    switch(path.toLowerCase()){
+        case '/':
+            response.writeHead(200);
+            message='You are on home page';
+            break;
+        case '/home':
+            response.writeHead(200);
+            message='You are on home page';
+            break;
+        case '/about':
+            response.writeHead(200);//setting status code.
+            message='You are on about section page';
+            break;
+        default:
+            response.writeHead(404);
+            message='Not Found';
+            break;
+            
+    }
+    response.end(message);
+
 });
 
 server.listen(8000,'127.0.0.1',()=>{
